105270b9523dbaff398076c5c195102470442d6c
shangxy <898013383@qq.com>
2019-09-16 16:24:11 +0800

    项目代码提交

---
+++ src/components/HostHistory.vue
@@ -0,0 +1,83 @@
+<template>
+</template>
+
+<script>
+
+    export default {
+        name: "HostHistory",
+        data() {
+            return {
+                echarts:{},
+                myCharts:{},
+                option:{
+                    title : {
+                        text: 'host状态',
+                        subtext: '纯属虚构'
+                    },
+                    tooltip : {
+                        trigger: 'axis'
+                    },
+                    legend: {
+                        data:['最高气温']
+                    },
+                    toolbox: {
+                        show : true,
+                        feature : {
+                            mark : {show: true},
+                            dataView : {show: true, readOnly: false},
+                            magicType : {show: true, type: ['line', 'bar']},
+                            restore : {show: true},
+                            saveAsImage : {show: true}
+                        }
+                    },
+                    calculable : true,
+                    xAxis : [
+                        {
+                            type : 'category',
+                            boundaryGap : false,
+                            data : ['10:00','11:00','12:00','13:00','14:00','15:00','16:00']
+                        }
+                    ],
+                    yAxis : [
+                        {
+                            type : 'value',
+                            axisLabel : {
+                                formatter: '{value} °C'
+                            }
+                        }
+                    ],
+                    series : [
+                        {
+                            name:'状态',
+                            type:'line',
+                            data:[0, 1, 0, 1,1, 0,1],
+                            markPoint : {
+                                data : [
+                                    {type : 'max', name: '最大值'},
+                                    {type : 'min', name: '最小值'}
+                                ]
+                            },
+
+                        },
+
+                    ]
+                }
+            }
+        },
+        created(){
+          this.initEcharts();
+        },
+        methods:{
+           initEcharts(){
+               this.echarts = require('echarts');
+               this.myCharts = this.echarts.init(document.getElementById("myChart"));
+               this.myCharts.setOption(this.option);
+           }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-container>
+    <el-container>
+      <el-header>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="1">host列表</el-menu-item>
+          <el-menu-item index="2">历史记录</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <host-main v-if="activeIndex == '1'"></host-main>
+        <host-history v-if="activeIndex == '2'"></host-history>
+        <div id="myChart"style="width: 100%;height: 300px"></div>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+    import HostMain from "./HostMain";
+    import HostHistory from "./HostHistory";
+    import $ from 'jquery'
+    export default {
+        name: 'Main',
+        components:{HostMain,HostHistory},
+        data() {
+            return {
+                activeIndex: "1",
+            }
+        },
+        methods: {
+            handleSelect(key) {
+                this.activeIndex = key;
+                if(key == '1'){
+                    $('#myChart').hide();
+                }else{
+                    $('#myChart').show();
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+  .el-header{
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+  }
+
+
+</style>
