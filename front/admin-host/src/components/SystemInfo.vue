ce3f44c4fa9dc531acaf86ac5b8121a2a00a8f38
lir <marjey_lee@163.com>
2019-09-28 10:25:46 +0800

    代码提交

--- front/admin-host/src/components/SystemInfo.vue
+++ front/admin-host/src/components/SystemInfo.vue
@@ -3,9 +3,9 @@
     <el-container>
       <el-header style="text-align: left; font-size: 12px">
         <label>系统名称：</label>
-        <el-input placeholder="" v-model="sysName" style="width: 150px"></el-input>
-        <label>管理员：</label>
         <el-input placeholder="" v-model="name" style="width: 150px"></el-input>
+        <label>管理员：</label>
+        <el-input placeholder="" v-model="manager" style="width: 150px"></el-input>
         <el-button type="primary" icon="el-icon-search" @click="search">查询</el-button>
         <el-button type="primary" icon="el-icon-refresh" @click="refresh">重置</el-button>
         <el-button type="primary" icon="el-icon-search" @click="add">新增</el-button>
@@ -13,12 +13,14 @@
       <el-main>
         <el-table
           :data="tableData"
+          v-loading="loading"
           height="98%"
-          style="width: 100%;">
+          style="width: 100%;"
+        >
           <el-table-column
             prop="name"
             label="系统名称"
-            min-width="23%">
+            min-width="30%">
           </el-table-column>
           <el-table-column
             prop="manager"
@@ -27,28 +29,28 @@
           </el-table-column>
           <el-table-column
             prop="telephone"
-            label="联系电话"
-            min-width="30%">
+            label="电话"
+            min-width="20%">
           </el-table-column>
           <el-table-column
             prop="job_number"
             label="工号"
-            min-width="13%">
+            min-width="20%">
           </el-table-column>
           <el-table-column
             fixed="right"
             label="操作"
-            min-width="13%">
+            width="150">
             <template slot-scope="scope">
               <el-button
-                @click.native.prevent="editorRow(scope.$index, tableData)"
+                @click.native.prevent="editorRow(scope.$index, scope.row)"
                 type="text"
                 icon="el-icon-edit"
                 size="small">
                 编辑
               </el-button>
               <el-button
-                @click.native.prevent="deleteRow(scope.$index, tableData)"
+                @click.native.prevent="deleteRow(scope.$index,tableData, scope.row)"
                 type="text"
                 icon="el-icon-delete"
                 size="small">
@@ -60,23 +62,22 @@
         <template>
           <el-dialog title="新增" :visible.sync="dialogFormVisible">
             <el-form ref="form" :model="form" label-width="80px">
-              <el-form-item label="IP">
-                <el-input v-model="form.ip"></el-input>
+              <el-form-item label="系统名称">
+                <el-input v-model="form.name"></el-input>
               </el-form-item>
-              <el-form-item label="端口">
-                <el-input v-model="form.port"></el-input>
+              <el-form-item label="管理员">
+                <el-input v-model="form.manager"></el-input>
               </el-form-item>
-              <el-form-item label="间隔">
-                <el-input v-model="form.timeNum"></el-input>
+              <el-form-item label="电话">
+                <el-input v-model="form.telephone"></el-input>
               </el-form-item>
-              <el-form-item label="描述">
-                <el-input type="textarea" v-model="form.describe"></el-input>
+              <el-form-item label="工号">
+                <el-input v-model="form.job_number"></el-input>
               </el-form-item>
-
             </el-form>
             <div slot="footer" class="dialog-footer">
               <el-button @click="dialogFormVisible = false">取 消</el-button>
-              <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+              <el-button type="primary" @click="handleSubmit">确 定</el-button>
             </div>
           </el-dialog>
         </template>
@@ -91,6 +92,7 @@
     export default {
         data() {
             return {
+                loading: false,
                 treeData: [],
                 tableData: [],
                 dateValue: [new Date(), new Date()],
@@ -101,45 +103,67 @@
                 currentPage: 4,
                 dialogFormVisible: false,
                 form: {
-                    ip: '',
-                    sys_name: '',
+                    id: '',
                     name: '',
+                    manager: '',
                     telephone: '',
                     job_number: '',
                     delivery: false,
                 },
                 formLabelWidth: '120px',
                 ip: '',
-                sysName: ''
+                name: '',
+                manager: '',
+                checkOptions: [{
+                    value: 'ping',
+                    label: 'ping'
+                }, {
+                    value: 'TCP',
+                    label: 'TCP'
+                }, {
+                    value: '半连接',
+                    label: '半连接'
+                }, {
+                    value: '自定义',
+                    label: '自定义'
+                }],
+                SysInfoOptions: [],
+                sysInfo: ''
             }
         },
         created() {
             this.getTreeInfo();
+            this.getSysInfoData();
         },
         methods: {
-            getTreeInfo() {
-                spuApi.getTreeData().then(res => {
-                    this.treeData = res.data.results.data;
+            deleteRow(index, rows, row) {
+                this.$confirm('是否确认删除?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    let params = {id: row.id};
+                    spuApi.deleteInstance(params).then(res => {
+                        rows.splice(index, 1);
+                        this.$message({
+                            type: 'success',
+                            message: '删除成功!'
+                        });
+                    })
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
                 });
             },
-            getTableList(sysId, check_type) {
-                spuApi.getTableDetails(sysId, check_type).then(res => {
-                    this.tableData = res.data;
-                })
-            },
-            handleNodeClick(data) {
-                this.getTableList(data.id, data.type);
-            },
-            deleteRow(index, rows) {
-                rows.splice(index, 1);
-            },
-            editorRow(index, rows) {
-                let data = rows[0];
-                this.form.ip = data.ip;
-                this.form.port = '';
-                this.form.type = data.type;
-                this.form.timeNum = data.interval;
-                this.form.describe = data.description;
+            editorRow(index, row) {
+                let data = row;
+                this.form.id = data.id;
+                this.form.name = data.name;
+                this.form.manager = data.manager;
+                this.form.telephone = data.telephone;
+                this.form.job_number = data.job_number;
                 this.dialogFormVisible = true;
                 console.log(this.form);
             },
@@ -154,21 +178,42 @@
                 this.form.port = '';
                 this.form.type = '';
                 this.form.timeNum = '';
+                this.form.sysInfo = '';
                 this.form.describe = '';
                 this.dialogFormVisible = true;
             },
             search() {
                 this.loading = true;
-                let params = {ip: this.ip, sys_name: this.sysName, check_type: this.checkType};
+                let params = {name: this.name, manager: this.manager};
                 spuApi.query_sys_info(params).then(res => {
                     this.tableData = res.data;
                     this.loading = false;
                 })
             },
+            handleSubmit() {
+                this.dialogFormVisible = false;
+                let params = {
+                    id: this.form.id,
+                    name: this.form.name,
+                    manager: this.form.manager,
+                    telephone: this.form.telephone,
+                    job_number: this.form.job_number,
+                    timeNum: this.form.timeNum,
+                    delivery: this.form.delivery,
+                    describe: this.form.describe,
+                };
+                spuApi.insert_or_update_system_info(params).then(
+                );
+            },
+            getSysInfoData() {
+                spuApi.query_sys_info({id: ""}).then(res => {
+                    this.SysInfoOptions = res.data
+                })
+            },
             refresh() {
-                this.sysName = '';
-                this.ip = '';
-                this.dateValue = [new Date(), new Date()]
+                this.name = '';
+                this.manager = '';
+                this.dateValue = [new Date(), new Date()];
             }
         }
     };
